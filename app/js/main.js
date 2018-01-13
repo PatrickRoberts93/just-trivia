4e4208b9770127565fdaca7cfb85d2726c49d056
Patrick <patrickroberts93@gmail.com>
2018-01-14 09:30:02 +1100

    Get categories from API and create category filters

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $( document ).ready(function() {
 
+  // GET THE CATEGORIES AND CREATE CATEGORY BUTTONS
+  $.ajax({url: "https://opentdb.com/api_category.php", success: function(data){
+    let categories = data.trivia_categories;
+    for (var i = 0; i < categories.length; i++) {
+      let html = "<button class='category' data-categoryid='"+categories[i].id+"'>"+categories[i].name+"</button>";
+      $('.categories').append(html);
+    }
+  }});
+
 });
