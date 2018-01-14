52a79c8af8e4320e12f476c2268ba577bb567950
Patrick Roberts <patrickroberts93@gmail.com>
2018-01-14 17:19:53 +1100

    Home page Desktop styling complete

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
   // GET THE CATEGORIES AND CREATE CATEGORY BUTTONS
   $.ajax({url: "https://opentdb.com/api_category.php", success: function(data){
     let categories = data.trivia_categories;
+    console.log(categories)
     for (var i = 0; i < categories.length; i++) {
       let html = "<button class='category' data-categoryid='"+categories[i].id+"'>"+categories[i].name+"</button>";
       $('.categories').append(html);
